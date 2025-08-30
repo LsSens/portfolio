86b589336b4eab547d5ef87696be58ff6299f893
Lucas Sens <lucassousasens@gmail.com>
2025-08-30 16:44:53 -0300

    feat: download CV

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -11,6 +11,10 @@ const HeroSection = () => {
     element?.scrollIntoView({ behavior: 'smooth' });
   };
 
+  const downloadCurriculum = () => {
+    window.open('https://portifoliolsens.s3.us-east-1.amazonaws.com/cv%20lucas%20sens.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAWUAXA3EDUVHUQXXV%2F20250830%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250830T194404Z&X-Amz-Expires=300&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEIT%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMSJIMEYCIQDhmA4NDS2RJzi%2FLf168%2BinGUOj%2BSI9%2F%2FPVVwr2N5cGsAIhAIEFAQnkCC25k45e0U8dM6g1yxOT6ntsjH1MuTQbFZ8eKt8CCN3%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQABoMNDU1MzE0ODIzNDMxIgwX4CaqHe7NL6IF35oqswJXwHEZEyGxW2tQ60NlPakeUo0ISN3KULilRyira1017SB4TNBIAO2P4jIXDUbqfrGmFjlTyDi75pZb230mBsx4PjS0IsLrrwhy2jdaqqi2lGDJhmEPWhZHl86JUKHAA83W5m7M0WKyw2HsbHuN6GxFtzSWHjoRqTdNFn%2BJxlZ3wdbqmI6FonDSccuz9FFj4chgHlrZ4Puih52JgcZixHhWMYTXV2mXZewgrXTi6668gUfSYi4%2FKLpVbbrhqnGbBFJtbbu%2B%2FMkSqJeFA6KEmbQOs0u6we4N5XSKczAcUuAepPXz04DxMamVvd%2BqFIoxKID%2FBnT3dWvuQlvtoxhi%2FcAkPdwhMju2ayRKowMlB0ZpSMBf%2BF%2F3qocimSXA79%2Fv4v9QQSS8q9ClC9UO92YuSxX7o7tNMLCozcUGOqwCZXcAP4mtSeSFlN7kn3sy2YptUHs1fi6SaihUQA9j1AcFCvnTfEhzIV3ybtY%2BYCrlhtY9c21L8IoxQnstVqST7OGiZB2cQwwCxZJL5TOWD0Rf36KKjPh0AEJP3KYLxyWTOxGOfLK0Sx5MlbyslSoXddzdmo6D88yFyS04tlaZGSSeQ8z3KaQyudkkF1buPiqNhVJjf1LKquBbjm%2BaMRx9ioxq6m5%2BlHlp3GtlLlJL3L%2BfxwKR8VEzVNfAt1TkNaeZakCm98mTqXdtbRBp8kgbgBO5DqkaewyEMC1ykf9ftdGdGRUJcA46IBXOWnnN2fgw%2Bq4XFgHFP1%2BLkqsoz1WFlJbN3HyqlP17FQirR2EBZR08YevizKtIsHt%2FRfKHt8K501n9hyH54BeVqSCo&X-Amz-Signature=a7610568bec5069d8edcfb6994f21d47c02dbc382aee40f8f61003b84981badc&X-Amz-SignedHeaders=host&response-content-disposition=inline', '_blank');
+  };
+
   return (
     <section id="hero" className="section-padding min-h-screen flex items-center">
       <div className="container-custom">
@@ -39,10 +43,10 @@ const HeroSection = () => {
               <Button 
                 size="lg" 
                 className="group"
-                onClick={() => scrollToSection('projects')}
+                onClick={() => downloadCurriculum()}
               >
                 <Download className="w-4 h-4 mr-2 group-hover:animate-bounce" />
-                Ver Projetos
+                Baixar curriculo
               </Button>
               
               <Button 
